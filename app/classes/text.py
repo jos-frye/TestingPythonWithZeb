4242782ab75602b24b2b135d144e1f606b1b156f
Jos Frye <jos.frye@pensardevelopment.com>
2019-12-12 22:04:30 -0800

    String length maths

---
+++ app/classes/text.py
@@ -0,0 +1,12 @@
+# when given a string, determine its lentgh 
+
+def stringlength(a):
+    return len(a)
+
+# When given a string and a number, return the character
+def character(a, n):
+    return a[n]
+
+print("The string length is:", stringlength("0123456"))
+
+print("The character number you entered is:", character("0123456", 2)) 
